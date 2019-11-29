723c5292fe6bd2f39009d64e018ac3e1f86e65c9
Ibrakov Azat <azatibrakov@gmail.com>
2019-11-30 00:35:20 +0300

    Complete build script

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
 python -m pip install --upgrade twine cibuildwheel
-CIBW_BEFORE_BUILD='python -m pip install -r requirements.txt'
-CIBW_BUILD=$(python -c 'import sys; print("cp" + "".join(map(str, sys.version_info[:2])) + "-*")')
+CIBW_BEFORE_BUILD='python -m pip install -r requirements.txt' \
+CIBW_BUILD=$(python -c 'import sys; print("cp" + "".join(map(str, sys.version_info[:2])) + "-*")') \
 cibuildwheel --output-dir dist
