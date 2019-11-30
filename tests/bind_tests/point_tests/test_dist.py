c61fe4c8d6a6c1a923b5e5138e56fefdd78b43b6
Azat Ibrakov <azatibrakov@gmail.com>
2019-11-30 04:22:55 +0300

    Add test for 'Point_2.dist' method

---
+++ tests/bind_tests/point_tests/test_dist.py
@@ -0,0 +1,29 @@
+from _martinez import Point_2
+from hypothesis import given
+
+from tests.utils import equivalence
+from . import strategies
+
+
+@given(strategies.points)
+def test_self(point: Point_2) -> None:
+    assert not point.dist(point)
+
+
+@given(strategies.points, strategies.points)
+def test_sign(first_point: Point_2, second_point: Point_2) -> None:
+    assert equivalence(first_point != second_point,
+                       first_point.dist(second_point) > 0)
+
+
+@given(strategies.points, strategies.points)
+def test_symmetry(first_point: Point_2, second_point: Point_2) -> None:
+    assert first_point.dist(second_point) == second_point.dist(first_point)
+
+
+@given(strategies.points, strategies.points, strategies.points)
+def test_triangle_inequality(first_point: Point_2,
+                             second_point: Point_2,
+                             third_point: Point_2) -> None:
+    assert (first_point.dist(second_point)
+            <= first_point.dist(third_point) + third_point.dist(second_point))
--- tests/strategies/literals.py
+++ tests/strategies/literals.py
@@ -1,7 +1,8 @@
 from hypothesis import strategies
 
 floats = strategies.floats(allow_nan=False,
-                           allow_infinity=True)
+                           allow_infinity=False)
 scalars = (strategies.integers() | floats
-           | strategies.decimals(allow_nan=False)
+           | strategies.decimals(allow_nan=False,
+                                 allow_infinity=False)
            | strategies.fractions())
