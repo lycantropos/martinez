b8338fd018b3225434a40846a4bb784bff15d83d
Azat Ibrakov <azatibrakov@gmail.com>
2019-11-30 03:25:12 +0300

    Complete 'Point' class: use read-only properties

--- martinez/point.py
+++ martinez/point.py
@@ -4,8 +4,18 @@ from .hints import Scalar
 
 
 class Point:
+    __slots__ = ('_x', '_y')
+
     def __init__(self, x: Scalar, y: Scalar) -> None:
-        self.x = x
-        self.y = y
+        self._x = x
+        self._y = y
 
     __repr__ = generate_repr(__init__)
+
+    @property
+    def x(self) -> Scalar:
+        return self._x
+
+    @property
+    def y(self) -> Scalar:
+        return self._y
