4dc3b17f59160675b6250d4f60cb5dd6bafd757a
Azat Ibrakov <azatibrakov@gmail.com>
2019-11-30 05:21:13 +0300

    Complete test for 'Point_2.dist' method

--- tests/bind_tests/point_tests/test_dist.py
+++ tests/bind_tests/point_tests/test_dist.py
@@ -1,3 +1,5 @@
+import math
+
 from _martinez import Point_2
 from hypothesis import given
 
@@ -25,5 +27,9 @@ def test_symmetry(first_point: Point_2, second_point: Point_2) -> None:
 def test_triangle_inequality(first_point: Point_2,
                              second_point: Point_2,
                              third_point: Point_2) -> None:
-    assert (first_point.dist(second_point)
-            <= first_point.dist(third_point) + third_point.dist(second_point))
+    straight_distance = first_point.dist(second_point)
+    workaround_distance = (first_point.dist(third_point)
+                           + third_point.dist(second_point))
+
+    assert (straight_distance <= workaround_distance
+            or math.isclose(straight_distance, workaround_distance))
