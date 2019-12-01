cd3ab3b518143e56445fbdabd477f808d2597938
Ibrakov Azat <azatibrakov@gmail.com>
2019-12-01 18:07:04 +0300

    Add test for bound 'BoundingBox.__add__' magic method

---
+++ tests/bind_tests/bounding_box_tests/test_add.py
@@ -0,0 +1,34 @@
+from _martinez import BoundingBox
+from hypothesis import given
+
+from . import strategies
+
+
+@given(strategies.bounding_boxes, strategies.bounding_boxes)
+def test_basic(first_bounding_box: BoundingBox,
+               second_bounding_box: BoundingBox) -> None:
+    result = first_bounding_box + second_bounding_box
+
+    assert isinstance(result, BoundingBox)
+
+
+@given(strategies.bounding_boxes)
+def test_idempotence(bounding_box: BoundingBox) -> None:
+    assert bounding_box + bounding_box == bounding_box
+
+
+@given(strategies.bounding_boxes, strategies.bounding_boxes)
+def test_commutativity(first_bounding_box: BoundingBox,
+                       second_bounding_box: BoundingBox) -> None:
+    assert (first_bounding_box + second_bounding_box
+            == second_bounding_box + first_bounding_box)
+
+
+@given(strategies.bounding_boxes,
+       strategies.bounding_boxes,
+       strategies.bounding_boxes)
+def test_associativity(first_bounding_box: BoundingBox,
+                       second_bounding_box: BoundingBox,
+                       third_bounding_box: BoundingBox) -> None:
+    assert ((first_bounding_box + second_bounding_box) + third_bounding_box
+            == first_bounding_box + (second_bounding_box + third_bounding_box))
