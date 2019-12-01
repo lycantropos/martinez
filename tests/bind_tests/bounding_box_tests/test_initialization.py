aeb7b696d707be9a9e591720fa341599af2a7448
Ibrakov Azat <azatibrakov@gmail.com>
2019-12-01 15:41:02 +0300

    Add test for 'BoundingBox' bound class

---
+++ tests/bind_tests/bounding_box_tests/test_initialization.py
@@ -0,0 +1,15 @@
+from _martinez import BoundingBox
+from hypothesis import given
+
+from tests import strategies
+
+
+@given(strategies.floats, strategies.floats,
+       strategies.floats, strategies.floats)
+def test_basic(x_min: float, x_max: float, y_min: float, y_max: float) -> None:
+    result = BoundingBox(x_min, y_min, x_max, y_max)
+
+    assert result.x_min == x_min
+    assert result.y_min == y_min
+    assert result.x_max == x_max
+    assert result.y_max == y_max
