03a21df7cc17af1af5ba189c2d63190fba3ce241
Azat Ibrakov <azatibrakov@gmail.com>
2019-12-02 15:19:25 +0300

    Add test for bound 'BoundingBox.__repr__' magic method

---
+++ tests/bind_tests/bounding_box_tests/test_repr.py
@@ -0,0 +1,21 @@
+import sys
+
+from _martinez import BoundingBox
+from hypothesis import given
+
+from . import strategies
+
+
+@given(strategies.bounding_boxes)
+def test_basic(bounding_box: BoundingBox) -> None:
+    result = repr(bounding_box)
+
+    assert result.startswith(BoundingBox.__module__)
+    assert BoundingBox.__qualname__ in result
+
+
+@given(strategies.bounding_boxes)
+def test_round_trip(bounding_box: BoundingBox) -> None:
+    result = repr(bounding_box)
+
+    assert eval(result, sys.modules) == bounding_box
