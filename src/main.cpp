9db6623d4f32951f575b3b800b7f30d0009e9b35
Azat Ibrakov <azatibrakov@gmail.com>
2019-12-02 15:37:52 +0300

    Complete module members order

--- src/main.cpp
+++ src/main.cpp
@@ -18,23 +18,6 @@ PYBIND11_MODULE(MODULE_NAME, m) {
         Python binding of polygon clipping algorithm by F. Martínez et al.
     )pbdoc";
 
-  py::class_<cbop::Point_2>(m, "Point")
-      .def(py::init<double, double>(), py::arg("x") = 0., py::arg("y") = 0.)
-      .def("__repr__",
-           [](const cbop::Point_2& self) {
-             std::ostringstream stream;
-             stream.precision(std::numeric_limits<double>::digits10);
-             stream << std::fixed << C_STR(MODULE_NAME) ".Point(" << self.x()
-                    << ", " << self.y() << ")";
-             return std::string(stream.str());
-           })
-      .def("__eq__", [](const cbop::Point_2& self,
-                        const cbop::Point_2& other) { return self == other; })
-      .def("distance_to", &cbop::Point_2::dist)
-      .def_property_readonly("x", &cbop::Point_2::x)
-      .def_property_readonly("y", &cbop::Point_2::y)
-      .def_property_readonly("bounding_box", &cbop::Point_2::bbox);
-
   py::class_<cbop::Bbox_2>(m, "BoundingBox")
       .def(py::init<double, double, double, double>(), py::arg("x_min") = 0.,
            py::arg("y_min") = 0., py::arg("x_max") = 0., py::arg("y_max") = 0.)
@@ -58,6 +41,23 @@ PYBIND11_MODULE(MODULE_NAME, m) {
       .def_property_readonly("x_max", &cbop::Bbox_2::xmax)
       .def_property_readonly("y_max", &cbop::Bbox_2::ymax)
       .def("__add__", &cbop::Bbox_2::operator+);
+
+  py::class_<cbop::Point_2>(m, "Point")
+      .def(py::init<double, double>(), py::arg("x") = 0., py::arg("y") = 0.)
+      .def("__repr__",
+           [](const cbop::Point_2& self) {
+             std::ostringstream stream;
+             stream.precision(std::numeric_limits<double>::digits10);
+             stream << std::fixed << C_STR(MODULE_NAME) ".Point(" << self.x()
+                    << ", " << self.y() << ")";
+             return std::string(stream.str());
+           })
+      .def("__eq__", [](const cbop::Point_2& self,
+                        const cbop::Point_2& other) { return self == other; })
+      .def("distance_to", &cbop::Point_2::dist)
+      .def_property_readonly("x", &cbop::Point_2::x)
+      .def_property_readonly("y", &cbop::Point_2::y)
+      .def_property_readonly("bounding_box", &cbop::Point_2::bbox);
 
 #ifdef VERSION_INFO
   m.attr("__version__") = VERSION_INFO;
