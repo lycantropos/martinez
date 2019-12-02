e5198db4bd19831c55ceec21bfa04b9305de9e1d
Azat Ibrakov <azatibrakov@gmail.com>
2019-12-02 18:09:24 +0300

    Add test for ported & bound 'Segment.max' property

---
+++ tests/integration_tests/segment_tests/test_max.py
@@ -0,0 +1,15 @@
+from typing import Tuple
+
+from _martinez import Segment as Bound
+from hypothesis import given
+
+from martinez.segment import Segment as Ported
+from tests.utils import are_bound_ported_points_equal
+from . import strategies
+
+
+@given(strategies.bound_with_ported_segments_pairs)
+def test_basic(bound_with_ported_segments_pair: Tuple[Bound, Ported]) -> None:
+    bound, ported = bound_with_ported_segments_pair
+
+    assert are_bound_ported_points_equal(bound.max, ported.max)
