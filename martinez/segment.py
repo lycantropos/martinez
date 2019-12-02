8c0e00b58a264472c106bad069ef3199101c2284
Azat Ibrakov <azatibrakov@gmail.com>
2019-12-02 17:49:23 +0300

    Complete ported 'Segment' class fields

--- martinez/segment.py
+++ martinez/segment.py
@@ -4,23 +4,15 @@ from .point import Point
 
 
 class Segment:
-    __slots__ = ('_source', '_target')
+    __slots__ = ('source', 'target')
 
     def __init__(self, source: Point, target: Point):
-        self._source = source
-        self._target = target
+        self.source = source
+        self.target = target
 
     __repr__ = generate_repr(__init__)
 
-    @property
-    def source(self) -> Point:
-        return self._source
-
-    @property
-    def target(self) -> Point:
-        return self._target
-
     def __eq__(self, other: 'Segment') -> bool:
-        return (self._source == other._source and self._target == other._target
+        return (self.source == other.source and self.target == other.target
                 if isinstance(other, Segment)
                 else NotImplemented)
