35e1bd6c9679a0d4ab837041424288adc449f141
Azat Ibrakov <azatibrakov@gmail.com>
2019-12-03 21:29:37 +0300

    Complete test for bound 'Contour.is_counterclockwise' property

--- tests/bind_tests/contour_tests/test_is_counterclockwise.py
+++ tests/bind_tests/contour_tests/test_is_counterclockwise.py
@@ -1,6 +1,7 @@
 from _martinez import Contour
 from hypothesis import given
 
+from tests.utils import implication
 from . import strategies
 
 
@@ -9,3 +10,17 @@ def test_basic(contour: Contour) -> None:
     result = contour.is_counterclockwise
 
     assert isinstance(result, bool)
+
+
+@given(strategies.contours)
+def test_empty(contour: Contour) -> None:
+    assert implication(not contour.points, contour.is_counterclockwise)
+
+
+@given(strategies.contours)
+def test_reversed(contour: Contour) -> None:
+    reversed_contour = Contour(contour.points[::-1], contour.holes,
+                               contour.is_external)
+
+    assert implication(bool(contour.points),
+                       contour.is_counterclockwise is not reversed_contour)
