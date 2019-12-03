f5bbf09c45f4feb4254525cb63a248c0285fd943
Azat Ibrakov <azatibrakov@gmail.com>
2019-12-03 15:14:19 +0300

    Add test for ported 'Contour.add_hole' method

--- tests/port_tests/contour_tests/strategies.py
+++ tests/port_tests/contour_tests/strategies.py
@@ -6,7 +6,8 @@ from tests.strategies import (scalars_strategies,
 
 points = scalars_strategies.flatmap(scalars_to_ported_points)
 points_lists = strategies.lists(points)
-non_negative_integers_lists = strategies.lists(strategies.integers(0))
+non_negative_integers = strategies.integers(0)
+non_negative_integers_lists = strategies.lists(non_negative_integers)
 booleans = strategies.booleans()
 contours = strategies.builds(Contour, points_lists,
                              non_negative_integers_lists, booleans)
---
+++ tests/port_tests/contour_tests/test_add_hole.py
@@ -0,0 +1,19 @@
+from hypothesis import given
+
+from martinez.contour import Contour
+from . import strategies
+
+
+@given(strategies.contours, strategies.non_negative_integers)
+def test_basic(contour: Contour, hole: int) -> None:
+    result = contour.add_hole(hole)
+
+    assert result is None
+
+
+@given(strategies.contours, strategies.non_negative_integers)
+def test_properties(contour: Contour, hole: int) -> None:
+    contour.add_hole(hole)
+
+    assert len(contour.holes) > 0
+    assert contour.holes[-1] == hole
