1150f5e258afea62dfac93f3420d7858c20115f1
Azat Ibrakov <azatibrakov@gmail.com>
2019-12-03 15:00:50 +0300

    Add test for ported & bound 'Contour.add' methods

---
+++ tests/integration_tests/contours_tests/test_add.py
@@ -0,0 +1,24 @@
+from typing import Tuple
+
+from _martinez import (Contour as Bound,
+                       Point as BoundPoint)
+from hypothesis import given
+
+from martinez.contour import Contour as Ported
+from martinez.point import Point as PortedPoint
+from tests.utils import are_bound_ported_points_equal
+from . import strategies
+
+
+@given(strategies.bound_with_ported_contours_pairs,
+       strategies.bound_with_ported_points_pairs)
+def test_basic(bound_with_ported_contours_pair: Tuple[Bound, Ported],
+               bound_with_ported_points_pair: Tuple[BoundPoint, PortedPoint]
+               ) -> None:
+    bound, ported = bound_with_ported_contours_pair
+    bound_point, ported_point = bound_with_ported_points_pair
+
+    bound.add(bound_point)
+    ported.add(ported_point)
+
+    assert are_bound_ported_points_equal(bound.points[-1], ported.points[-1])
