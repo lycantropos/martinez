9f827e132fe02af149c54ca41e6db4cc11f4684a
Azat Ibrakov <azatibrakov@gmail.com>
2019-12-03 14:11:13 +0300

    Add test for ported 'Contour.__eq__' magic method

---
+++ tests/port_tests/contour_tests/test_equals.py
@@ -0,0 +1,33 @@
+from hypothesis import given
+
+from martinez.contour import Contour
+from tests.utils import (equivalence,
+                         implication)
+from . import strategies
+
+
+@given(strategies.contours)
+def test_reflexivity(contour: Contour) -> None:
+    assert contour == contour
+
+
+@given(strategies.contours, strategies.contours)
+def test_symmetry(first_contour: Contour, second_contour: Contour) -> None:
+    assert equivalence(first_contour == second_contour,
+                       second_contour == first_contour)
+
+
+@given(strategies.contours, strategies.contours, strategies.contours)
+def test_transitivity(first_contour: Contour,
+                      second_contour: Contour,
+                      third_contour: Contour) -> None:
+    assert implication(first_contour == second_contour
+                       and second_contour == third_contour,
+                       first_contour == third_contour)
+
+
+@given(strategies.contours, strategies.contours)
+def test_connection_with_inequality(first_contour: Contour,
+                                    second_contour: Contour) -> None:
+    assert equivalence(not first_contour == second_contour,
+                       first_contour != second_contour)
