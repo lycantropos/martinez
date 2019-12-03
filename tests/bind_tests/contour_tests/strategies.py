9c3d31088939e4b8228f05252f62491857c1d2a0
Azat Ibrakov <azatibrakov@gmail.com>
2019-12-03 15:12:14 +0300

    Add test for bound 'Contour.add_hole' method

--- tests/bind_tests/contour_tests/strategies.py
+++ tests/bind_tests/contour_tests/strategies.py
@@ -6,7 +6,8 @@ from tests.strategies import floats
 
 points = strategies.builds(Point, floats, floats)
 points_lists = strategies.lists(points)
-non_negative_integers_lists = strategies.lists(strategies.integers(0, 65535))
+non_negative_integers = strategies.integers(0, 65535)
+non_negative_integers_lists = strategies.lists(non_negative_integers)
 booleans = strategies.booleans()
 contours = strategies.builds(Contour, points_lists,
                              non_negative_integers_lists, booleans)
