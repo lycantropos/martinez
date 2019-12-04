9b519fb937f89590b606f0fff1c7ba96b1889d17
Azat Ibrakov <azatibrakov@gmail.com>
2019-12-04 09:11:10 +0300

    Add test for ported 'Contour.is_clockwise' property

---
+++ tests/port_tests/contour_tests/test_is_clockwise.py
@@ -0,0 +1,31 @@
+from hypothesis import given
+
+from martinez.contour import Contour
+from tests.utils import implication
+from . import strategies
+
+
+@given(strategies.contours)
+def test_basic(contour: Contour) -> None:
+    result = contour.is_clockwise
+
+    assert isinstance(result, bool)
+
+
+@given(strategies.contours)
+def test_empty(contour: Contour) -> None:
+    assert implication(not contour.points, not contour.is_clockwise)
+
+
+@given(strategies.contours)
+def test_reversed(contour: Contour) -> None:
+    reversed_contour = Contour(contour.points[::-1], contour.holes,
+                               contour.is_external)
+
+    assert implication(bool(contour.points),
+                       contour.is_clockwise is not reversed_contour)
+
+
+@given(strategies.contours)
+def test_alternatives(contour: Contour) -> None:
+    assert implication(contour.is_clockwise, not contour.is_counterclockwise)
