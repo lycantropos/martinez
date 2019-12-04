ffaf982b757ce45e7d792b85074f90484184313c
Ibrakov Azat <azatibrakov@gmail.com>
2019-12-04 22:58:51 +0300

    Complete test for bound 'Contour.set_clockwise' method

--- tests/bind_tests/contour_tests/test_set_clockwise.py
+++ tests/bind_tests/contour_tests/test_set_clockwise.py
@@ -1,19 +1,11 @@
 from _martinez import Contour
 from hypothesis import given
 
-from tests.utils import implication
 from . import strategies
 
 
 @given(strategies.contours)
 def test_basic(contour: Contour) -> None:
-    result = contour.reverse()
+    result = contour.set_clockwise()
 
     assert result is None
-
-
-@given(strategies.contours)
-def test_orientation(contour: Contour) -> None:
-    contour.set_clockwise()
-
-    assert implication(len(contour.points) > 1, contour.is_clockwise)
