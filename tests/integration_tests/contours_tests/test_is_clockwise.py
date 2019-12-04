b5258ba32d2834c8939ed84ba45ad7ad5dbf13d2
Azat Ibrakov <azatibrakov@gmail.com>
2019-12-04 09:12:55 +0300

    Add test for bound & ported 'Contour.is_clockwise' properties

---
+++ tests/integration_tests/contours_tests/test_is_clockwise.py
@@ -0,0 +1,14 @@
+from typing import Tuple
+
+from _martinez import Contour as Bound
+from hypothesis import given
+
+from martinez.contour import Contour as Ported
+from . import strategies
+
+
+@given(strategies.bound_with_ported_contours_pairs)
+def test_basic(bound_with_ported_contours_pair: Tuple[Bound, Ported]) -> None:
+    bound, ported = bound_with_ported_contours_pair
+
+    assert bound.is_clockwise is ported.is_clockwise
