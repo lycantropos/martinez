b354c5fccde65b0bf7ff73005c57f8eabe466e50
Azat Ibrakov <azatibrakov@gmail.com>
2019-12-04 21:13:30 +0300

    Add test for bound & ported 'Contour.reverse' methods

---
+++ tests/integration_tests/contours_tests/test_reverse.py
@@ -0,0 +1,18 @@
+from typing import Tuple
+
+from _martinez import Contour as Bound
+from hypothesis import given
+
+from martinez.contour import Contour as Ported
+from tests.utils import are_bound_ported_contours_equal
+from . import strategies
+
+
+@given(strategies.bound_with_ported_contours_pairs)
+def test_basic(bound_with_ported_contours_pair: Tuple[Bound, Ported]) -> None:
+    bound, ported = bound_with_ported_contours_pair
+
+    bound.reverse()
+    ported.reverse()
+
+    assert are_bound_ported_contours_equal(bound, ported)
