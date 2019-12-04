c0dafd21afe7bfd93c853ee004e1212f802fadd4
Azat Ibrakov <azatibrakov@gmail.com>
2019-12-04 19:47:06 +0300

    Add test for bound & ported 'BoundingBox' class copying

---
+++ tests/integration_tests/bounding_boxes_tests/test_copy.py
@@ -0,0 +1,25 @@
+import copy
+from typing import Tuple
+
+from _martinez import BoundingBox as Bound
+from hypothesis import given
+
+from martinez.bounding_box import BoundingBox as Ported
+from tests.utils import are_bound_ported_bounding_boxes_equal
+from . import strategies
+
+
+@given(strategies.bound_with_ported_bounding_boxes_pairs)
+def test_shallow(bound_with_ported_boxes_pair: Tuple[Bound, Ported]) -> None:
+    bound, ported = bound_with_ported_boxes_pair
+
+    assert are_bound_ported_bounding_boxes_equal(copy.copy(bound),
+                                                 copy.copy(ported))
+
+
+@given(strategies.bound_with_ported_bounding_boxes_pairs)
+def test_deep(bound_with_ported_boxes_pair: Tuple[Bound, Ported]) -> None:
+    bound, ported = bound_with_ported_boxes_pair
+
+    assert are_bound_ported_bounding_boxes_equal(copy.deepcopy(bound),
+                                                 copy.deepcopy(ported))
