ca9eb41613e375ca650aaf9786374c6a7eaef477
Azat Ibrakov <azatibrakov@gmail.com>
2019-12-05 09:05:26 +0300

    Add test for ported 'Polygon.__iter__' magic method

---
+++ tests/port_tests/polygon_tests/test_iteration.py
@@ -0,0 +1,29 @@
+from hypothesis import given
+
+from martinez.contour import Contour
+from martinez.polygon import Polygon
+from tests.utils import capacity
+from . import strategies
+
+
+@given(strategies.polygons)
+def test_basic(polygon: Polygon) -> None:
+    result = iter(polygon)
+
+    assert all(isinstance(element, Contour)
+               for element in result)
+
+
+@given(strategies.polygons)
+def test_elements(polygon: Polygon) -> None:
+    result = iter(polygon)
+
+    assert all(element in polygon.contours
+               for element in result)
+
+
+@given(strategies.polygons)
+def test_size(polygon: Polygon) -> None:
+    result = iter(polygon)
+
+    assert capacity(result) == len(polygon.contours)
