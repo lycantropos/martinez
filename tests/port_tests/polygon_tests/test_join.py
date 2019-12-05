b06eaaea3397f5ced9fe376ae49a9150f9fa55dc
Azat Ibrakov <azatibrakov@gmail.com>
2019-12-05 11:12:05 +0300

    Add test for ported 'Polygon.join' method

---
+++ tests/port_tests/polygon_tests/test_join.py
@@ -0,0 +1,37 @@
+import copy
+
+from hypothesis import given
+
+from martinez.polygon import Polygon
+from . import strategies
+
+
+@given(strategies.polygons, strategies.polygons)
+def test_basic(first_polygon: Polygon, second_polygon: Polygon) -> None:
+    result = first_polygon.join(second_polygon)
+
+    assert result is None
+
+
+@given(strategies.polygons, strategies.polygons)
+def test_size(first_polygon: Polygon, second_polygon: Polygon) -> None:
+    original_first_polygon = copy.deepcopy(first_polygon)
+
+    first_polygon.join(second_polygon)
+
+    assert len(first_polygon.contours) == (len(original_first_polygon.contours)
+                                           + len(second_polygon.contours))
+
+
+@given(strategies.polygons, strategies.polygons)
+def test_elements(first_polygon: Polygon, second_polygon: Polygon) -> None:
+    first_polygon.join(second_polygon)
+
+    assert all(first_contour.points == second_contour.points
+               for first_contour, second_contour
+               in zip(first_polygon.contours[-len(second_polygon.contours):],
+                      second_polygon.contours))
+    assert all(first_contour.is_external is second_contour.is_external
+               for first_contour, second_contour
+               in zip(first_polygon.contours[-len(second_polygon.contours):],
+                      second_polygon.contours))
