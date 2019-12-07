c4681a74bb9c35e4752fdc59cac6e755618a2649
Ibrakov Azat <azatibrakov@gmail.com>
2019-12-07 14:03:59 +0300

    Complete test for ported 'sign' function

--- tests/port_tests/utilities_tests/test_sign.py
+++ tests/port_tests/utilities_tests/test_sign.py
@@ -1,18 +1,16 @@
 from itertools import permutations
-from typing import Tuple
 
 from hypothesis import given
 
-from martinez.point import Point
 from martinez.utilities import sign
-from tests.utils import (equivalence,
+from tests.utils import (PortedPointsTriplet,
                          is_even_permutation,
                          permute)
 from . import strategies
 
 
 @given(strategies.points_triplets)
-def test_basic(points_triplet: Tuple[Point, Point, Point]) -> None:
+def test_basic(points_triplet: PortedPointsTriplet) -> None:
     first_point, second_point, third_point = points_triplet
 
     result = sign(first_point, second_point, third_point)
@@ -21,15 +19,12 @@ def test_basic(points_triplet: Tuple[Point, Point, Point]) -> None:
 
 
 @given(strategies.points_triplets)
-def test_permutations(points_triplet: Tuple[Point, Point, Point]) -> None:
+def test_permutations(points_triplet: PortedPointsTriplet) -> None:
     first_point, second_point, third_point = points_triplet
 
     result = sign(first_point, second_point, third_point)
 
     points = [first_point, second_point, third_point]
-    assert (all(sign(*points) == result
-                for points in permutations(points))
-            if result == 0
-            else all(equivalence(is_even_permutation(permutation),
-                                 sign(*permute(points, permutation)) == result)
-                     for permutation in permutations(range(3))))
+    assert all(sign(*permute(points, permutation)) ==
+               (result if is_even_permutation(permutation) else -result)
+               for permutation in permutations(range(3)))
