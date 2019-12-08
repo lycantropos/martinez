fa727fef28b5a0a12a9ad7c23279fca2698d9358
Ibrakov Azat <azatibrakov@gmail.com>
2019-12-09 00:07:26 +0300

    Complete 'Segment_2' class fields

--- src/segment_2.h
+++ src/segment_2.h
@@ -26,36 +26,42 @@ class Segment_2 {
 
   /** Constructor from two points **/
   Segment_2(const Point_2& source, const Point_2& target)
-      : s(source), t(target) {}
+      : _source(source), _target(target) {}
 
   /** Set the source point */
-  void setSource(const Point_2& source) { s = source; }
+  void setSource(const Point_2& source) { _source = source; }
   /** Set the target point */
-  void setTarget(const Point_2& target) { t = target; }
+  void setTarget(const Point_2& target) { _target = target; }
 
   /** Get the source point */
-  const Point_2& source() const { return s; }
+  const Point_2& source() const { return _source; }
   /** Get the target point */
-  const Point_2& target() const { return t; }
+  const Point_2& target() const { return _target; }
 
   /** Return the point of the segment with lexicographically smallest coordinate
    */
   const Point_2& min() const {
-    return (s.x() < t.x()) || (s.x() == t.x() && s.y() < t.y()) ? s : t;
+    return (_source.x() < _target.x()) ||
+                   (_source.x() == _target.x() && _source.y() < _target.y())
+               ? _source
+               : _target;
   }
   /** Return the point of the segment with lexicographically largest coordinate
    */
   const Point_2& max() const {
-    return (s.x() > t.x()) || (s.x() == t.x() && s.y() > t.y()) ? s : t;
+    return (_source.x() > _target.x()) ||
+                   (_source.x() == _target.x() && _source.y() > _target.y())
+               ? _source
+               : _target;
   }
-  bool degenerate() const { return s == t; }
-  bool is_vertical() const { return s.x() == t.x(); }
+  bool degenerate() const { return _source == _target; }
+  bool is_vertical() const { return _source.x() == _target.x(); }
   /** Change the segment orientation */
-  Segment_2 changeOrientation() { return Segment_2(t, s); }
+  Segment_2 changeOrientation() { return Segment_2(_target, _source); }
 
  private:
   /** Segment endpoints */
-  Point_2 s, t;
+  Point_2 _source, _target;
 };
 
 inline std::ostream& operator<<(std::ostream& o, const Segment_2& p) {
