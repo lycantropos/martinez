f4aeddc9fd42344c06206b1369d8da0ad14b5b36
Ibrakov Azat <azatibrakov@gmail.com>
2019-12-08 23:50:00 +0300

    Complete test for bound 'find_intersections' function

--- tests/bind_tests/utilities_tests/test_find_intersections.py
+++ tests/bind_tests/utilities_tests/test_find_intersections.py
@@ -25,3 +25,12 @@ def test_same_segment(segment: Segment) -> None:
     assert result[0] == (1 if segment.is_degenerate else 2)
     assert result[1] == segment.source
     assert implication(segment.is_degenerate, result[-1] is None)
+
+
+@given(strategies.segments)
+def test_reversed(segment: Segment) -> None:
+    result = find_intersections(segment, segment.reversed)
+
+    assert result[0] == (1 if segment.is_degenerate else 2)
+    assert result[1] == segment.source
+    assert implication(segment.is_degenerate, result[-1] is None)
