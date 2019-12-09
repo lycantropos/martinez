735447439c582a493e412eb71000370be3b833b8
Azat Ibrakov <azatibrakov@gmail.com>
2019-12-09 18:41:44 +0300

    Add test for ported 'SweepEvent.__repr__' magic method

--- tests/port_tests/boolean_tests/sweep_event_tests/strategies.py
+++ tests/port_tests/boolean_tests/sweep_event_tests/strategies.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from hypothesis import strategies
 
 from martinez.boolean import (EdgeType,
@@ -14,14 +16,18 @@ points = scalars_strategies.flatmap(scalars_to_ported_points)
 polygons_types = strategies.sampled_from(list(PolygonType.__members__
                                               .values()))
 edges_types = strategies.sampled_from(list(EdgeType.__members__.values()))
-deferred_sweep_events = strategies.deferred(lambda: sweep_events)
-maybe_sweep_events = strategies.none() | deferred_sweep_events
 
 
-def scalars_to_sweep_events(scalars: Strategy[Scalar]) -> SweepEvent:
-    return strategies.builds(SweepEvent, booleans,
-                             strategies.builds(Point, scalars, scalars),
-                             maybe_sweep_events, polygons_types, edges_types)
+def scalars_to_sweep_events(scalars: Strategy[Scalar]) -> Strategy[SweepEvent]:
+    def from_children(children: Strategy[Optional[SweepEvent]]
+                      ) -> Strategy[SweepEvent]:
+        return strategies.builds(SweepEvent, booleans,
+                                 strategies.builds(Point, scalars, scalars),
+                                 children, polygons_types, edges_types)
+
+    return strategies.recursive(from_children(strategies.none()),
+                                from_children)
 
 
 sweep_events = scalars_strategies.flatmap(scalars_to_sweep_events)
+maybe_sweep_events = strategies.none() | sweep_events
---
+++ tests/port_tests/boolean_tests/sweep_event_tests/test_repr.py
@@ -0,0 +1,21 @@
+from hypothesis import given
+
+from martinez import boolean
+from martinez.boolean import SweepEvent
+from . import strategies
+
+
+@given(strategies.sweep_events)
+def test_basic(sweep_event: SweepEvent) -> None:
+    result = repr(sweep_event)
+
+    assert result.startswith(SweepEvent.__qualname__)
+
+
+@given(strategies.sweep_events)
+def test_round_trip(sweep_event: SweepEvent) -> None:
+    result = repr(sweep_event)
+
+    scalar_type = type(sweep_event.point.x)
+    assert eval(result, {**vars(boolean),
+                         scalar_type.__qualname__: scalar_type}) == sweep_event
