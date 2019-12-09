00201a28dc4298e18a3982d0af16d3bc465ac9aa
Azat Ibrakov <azatibrakov@gmail.com>
2019-12-09 17:27:59 +0300

    Add test for bound 'SweepEvent.__eq__' magic method

--- tests/bind_tests/boolean_tests/sweep_event_tests/strategies.py
+++ tests/bind_tests/boolean_tests/sweep_event_tests/strategies.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from _martinez import (EdgeType,
                        Point,
                        PolygonType,
@@ -8,6 +10,7 @@ from tests.strategies import (booleans,
                               floats,
                               unsigned_integers,
                               unsigned_integers_lists)
+from tests.utils import Strategy
 
 booleans = booleans
 non_negative_integers = unsigned_integers
@@ -16,8 +19,22 @@ points = strategies.builds(Point, floats, floats)
 polygons_types = strategies.sampled_from(list(PolygonType.__members__
                                               .values()))
 edges_types = strategies.sampled_from(list(EdgeType.__members__.values()))
-deferred_sweep_events = strategies.deferred(lambda: sweep_events)
-maybe_sweep_events = strategies.none() | deferred_sweep_events
-sweep_events = strategies.builds(SweepEvent, booleans, points,
-                                 maybe_sweep_events,
+
+
+def extend_sweep_events(strategy: Strategy[Optional[SweepEvent]]
+                        ) -> Strategy[SweepEvent]:
+    def expand(sweep_event: Optional[SweepEvent]) -> Strategy[SweepEvent]:
+        return strategies.builds(SweepEvent, booleans, points,
+                                 strategies.just(sweep_event),
                                  polygons_types, edges_types)
+
+    return strategy.flatmap(expand)
+
+
+sweep_events = strategies.recursive(strategies.builds(SweepEvent, booleans,
+                                                      points,
+                                                      strategies.none(),
+                                                      polygons_types,
+                                                      edges_types),
+                                    extend_sweep_events)
+maybe_sweep_events = strategies.none() | sweep_events
