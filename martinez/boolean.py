74759196b47a0e2ad4bcf7e1a12bf3f49301ba0b
Azat Ibrakov <azatibrakov@gmail.com>
2019-12-09 13:15:21 +0300

    Add ported 'EdgeType' enum

--- martinez/boolean.py
+++ martinez/boolean.py
@@ -2,6 +2,14 @@ import enum
 
 
 @enum.unique
+class EdgeType(enum.IntEnum):
+    NORMAL = 0
+    NON_CONTRIBUTING = 1
+    SAME_TRANSITION = 2
+    DIFFERENT_TRANSITION = 3
+
+
+@enum.unique
 class OperationType(enum.IntEnum):
     INTERSECTION = 0
     UNION = 1
