a1a647a3a0a693a8e786966e5eb10f81efbf7d58
Azat Ibrakov <azatibrakov@gmail.com>
2019-12-09 17:37:17 +0300

    Add test for ported 'SweepEvent.__eq__' magic method

---
+++ tests/port_tests/boolean_tests/sweep_event_tests/test_equals.py
@@ -0,0 +1,35 @@
+from hypothesis import given
+
+from martinez.boolean import SweepEvent
+from tests.utils import (equivalence,
+                         implication)
+from . import strategies
+
+
+@given(strategies.sweep_events)
+def test_reflexivity(sweep_event: SweepEvent) -> None:
+    assert sweep_event == sweep_event
+
+
+@given(strategies.sweep_events, strategies.sweep_events)
+def test_symmetry(first_sweep_event: SweepEvent,
+                  second_sweep_event: SweepEvent) -> None:
+    assert equivalence(first_sweep_event == second_sweep_event,
+                       second_sweep_event == first_sweep_event)
+
+
+@given(strategies.sweep_events, strategies.sweep_events,
+       strategies.sweep_events)
+def test_transitivity(first_sweep_event: SweepEvent,
+                      second_sweep_event: SweepEvent,
+                      third_sweep_event: SweepEvent) -> None:
+    assert implication(first_sweep_event == second_sweep_event
+                       and second_sweep_event == third_sweep_event,
+                       first_sweep_event == third_sweep_event)
+
+
+@given(strategies.sweep_events, strategies.sweep_events)
+def test_connection_with_inequality(first_sweep_event: SweepEvent,
+                                    second_sweep_event: SweepEvent) -> None:
+    assert equivalence(not first_sweep_event == second_sweep_event,
+                       first_sweep_event != second_sweep_event)
