20d9117d1a9e0501cccdb857f8f784d4dd1f93e5
Azat Ibrakov <azatibrakov@gmail.com>
2019-12-10 17:16:08 +0300

    Add test for bound 'SweepEvent.is_vertical' property

--- tests/bind_tests/boolean_tests/sweep_event_tests/strategies.py
+++ tests/bind_tests/boolean_tests/sweep_event_tests/strategies.py
@@ -27,4 +27,11 @@ leaf_sweep_events = strategies.builds(SweepEvent, booleans, points,
                                       strategies.none(),
                                       polygons_types, edges_types)
 sweep_events = strategies.recursive(leaf_sweep_events, extend_sweep_events)
+
+
+def is_nested_sweep_event(sweep_event: SweepEvent) -> bool:
+    return sweep_event.other_event is not None
+
+
+nested_sweep_events = sweep_events.filter(is_nested_sweep_event)
 maybe_sweep_events = strategies.none() | sweep_events
