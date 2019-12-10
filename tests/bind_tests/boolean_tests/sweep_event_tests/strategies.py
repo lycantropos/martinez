f6075da690dd4620627b7c1773c839e145dae830
Azat Ibrakov <azatibrakov@gmail.com>
2019-12-10 17:03:26 +0300

    Complete strategies

--- tests/bind_tests/boolean_tests/sweep_event_tests/strategies.py
+++ tests/bind_tests/boolean_tests/sweep_event_tests/strategies.py
@@ -19,12 +19,8 @@ edges_types = strategies.sampled_from(list(EdgeType.__members__.values()))
 
 def extend_sweep_events(strategy: Strategy[Optional[SweepEvent]]
                         ) -> Strategy[SweepEvent]:
-    def expand(sweep_event: Optional[SweepEvent]) -> Strategy[SweepEvent]:
-        return strategies.builds(SweepEvent, booleans, points,
-                                 strategies.just(sweep_event),
-                                 polygons_types, edges_types)
-
-    return strategy.flatmap(expand)
+    return strategies.builds(SweepEvent, booleans, points,
+                             strategy, polygons_types, edges_types)
 
 
 leaf_sweep_events = strategies.builds(SweepEvent, booleans, points,
