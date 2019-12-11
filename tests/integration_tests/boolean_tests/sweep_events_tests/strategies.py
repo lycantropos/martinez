ec82d0812e6c6aa5143ce9d89c696205338d71a9
Azat Ibrakov <azatibrakov@gmail.com>
2019-12-11 07:40:26 +0300

    Complete test for bound & ported 'SweepEvent' clases initializations

--- tests/integration_tests/boolean_tests/sweep_events_tests/strategies.py
+++ tests/integration_tests/boolean_tests/sweep_events_tests/strategies.py
@@ -1,12 +1,21 @@
+from itertools import repeat
+from typing import (Optional,
+                    Tuple)
+
 from _martinez import (EdgeType as BoundEdgeType,
-                       PolygonType as BoundPolygonType)
+                       Point as BoundPoint,
+                       PolygonType as BoundPolygonType,
+                       SweepEvent as Bound)
 from hypothesis import strategies
 
 from martinez.boolean import (EdgeType as PortedEdgeType,
-                              PolygonType as PortedPolygonType)
+                              PolygonType as PortedPolygonType,
+                              SweepEvent as Ported)
+from martinez.point import Point as PortedPoint
 from tests.strategies import (booleans,
                               single_precision_floats as floats,
                               to_bound_with_ported_points_pair)
+from tests.utils import Strategy
 
 booleans = booleans
 bound_with_ported_points_pairs = strategies.builds(
@@ -21,3 +30,47 @@ bound_with_ported_edges_types_pairs = strategies.sampled_from(
           PortedEdgeType.__members__[name])
          for name in (BoundEdgeType.__members__.keys() &
                       PortedEdgeType.__members__.keys())])
+
+
+def to_bound_with_ported_sweep_events(
+        bound_with_ported_children: Strategy[Tuple[Optional[Bound],
+                                                   Optional[Ported]]]
+) -> Strategy[Tuple[Bound, Ported]]:
+    def to_sweep_events(
+            is_left: bool,
+            bound_with_ported_points_pair: Tuple[BoundPoint, PortedPoint],
+            bound_with_ported_other_events_pair: Tuple[Bound, Ported],
+            bound_with_ported_polygons_types_pair: Tuple[BoundPolygonType,
+                                                         PortedPolygonType],
+            bound_with_ported_edges_types_pair: Tuple[BoundEdgeType,
+                                                      PortedEdgeType]
+    ) -> Tuple[Bound, Ported]:
+        bound_point, ported_point = bound_with_ported_points_pair
+        (bound_other_event,
+         ported_other_event) = bound_with_ported_other_events_pair
+        (bound_polygon_type,
+         ported_polygon_type) = bound_with_ported_polygons_types_pair
+        bound_edge_type, ported_edge_type = bound_with_ported_edges_types_pair
+
+        bound = Bound(is_left, bound_point, bound_other_event,
+                      bound_polygon_type,
+                      bound_edge_type)
+        ported = Ported(is_left, ported_point, ported_other_event,
+                        ported_polygon_type, ported_edge_type)
+        return bound, ported
+
+    return strategies.builds(to_sweep_events,
+                             booleans, bound_with_ported_points_pairs,
+                             bound_with_ported_children,
+                             bound_with_ported_polygons_types_pairs,
+                             bound_with_ported_edges_types_pairs)
+
+
+nones_pairs = strategies.tuples(*repeat(strategies.none(), 2))
+bound_with_ported_leaf_sweep_events_pairs = to_bound_with_ported_sweep_events(
+        nones_pairs)
+bound_with_ported_sweep_events_pairs = strategies.recursive(
+        bound_with_ported_leaf_sweep_events_pairs,
+        to_bound_with_ported_sweep_events)
+bound_with_ported_maybe_sweep_events_pairs = (
+        nones_pairs | bound_with_ported_sweep_events_pairs)
--- tests/integration_tests/boolean_tests/sweep_events_tests/test_initialization.py
+++ tests/integration_tests/boolean_tests/sweep_events_tests/test_initialization.py
@@ -1,4 +1,5 @@
-from typing import Tuple
+from typing import (Optional,
+                    Tuple)
 
 from _martinez import (EdgeType as BoundEdgeType,
                        Point as BoundPoint,
@@ -16,23 +17,27 @@ from . import strategies
 
 @given(strategies.booleans,
        strategies.bound_with_ported_points_pairs,
+       strategies.bound_with_ported_maybe_sweep_events_pairs,
        strategies.bound_with_ported_polygons_types_pairs,
        strategies.bound_with_ported_edges_types_pairs)
 def test_basic(is_left: bool,
                bound_with_ported_points_pair: Tuple[BoundPoint, PortedPoint],
+               bound_with_ported_other_events_pair: Tuple[Optional[Bound],
+                                                          Optional[Ported]],
                bound_with_ported_polygons_types_pair: Tuple[BoundPolygonType,
                                                             PortedPolygonType],
                bound_with_ported_edges_types_pair: Tuple[BoundEdgeType,
                                                          PortedEdgeType],
                ) -> None:
     bound_point, ported_point = bound_with_ported_points_pair
+    bound_other_event, ported_other_event = bound_with_ported_other_events_pair
     (bound_polygon_type,
      ported_polygon_type) = bound_with_ported_polygons_types_pair
     bound_edge_type, ported_edge_type = bound_with_ported_edges_types_pair
 
-    bound = Bound(is_left, bound_point, None, bound_polygon_type,
+    bound = Bound(is_left, bound_point, bound_other_event, bound_polygon_type,
                   bound_edge_type)
-    ported = Ported(is_left, ported_point, None, ported_polygon_type,
-                    ported_edge_type)
+    ported = Ported(is_left, ported_point, ported_other_event,
+                    ported_polygon_type, ported_edge_type)
 
     assert are_bound_ported_sweep_events_equal(bound, ported)
