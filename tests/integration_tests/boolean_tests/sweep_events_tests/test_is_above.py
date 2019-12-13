cc9ea8fa952c094e645fc5c4b4a32578a1064263
Azat Ibrakov <azatibrakov@gmail.com>
2019-12-13 12:52:07 +0300

    Add test for bound & ported 'SweepEvent.is_above' method

---
+++ tests/integration_tests/boolean_tests/sweep_events_tests/test_is_above.py
@@ -0,0 +1,20 @@
+from typing import Tuple
+
+from _martinez import (Point as BoundPoint,
+                       SweepEvent as Bound)
+from hypothesis import given
+
+from martinez.boolean import SweepEvent as Ported
+from martinez.point import Point as PortedPoint
+from . import strategies
+
+
+@given(strategies.bound_with_ported_nested_sweep_events_pairs,
+       strategies.bound_with_ported_points_pairs)
+def test_basic(bound_with_ported_sweep_events_pair: Tuple[Bound, Ported],
+               bound_with_ported_points_pair: Tuple[BoundPoint, PortedPoint],
+               ) -> None:
+    bound, ported = bound_with_ported_sweep_events_pair
+    bound_point, ported_point = bound_with_ported_points_pair
+
+    assert bound.is_above(bound_point) is ported.is_above(ported_point)
