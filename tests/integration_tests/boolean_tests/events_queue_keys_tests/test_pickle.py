54e3e14bbf9ba948b9393465a41d778c3b21cb12
Azat Ibrakov <azatibrakov@gmail.com>
2019-12-14 03:18:02 +0300

    Add test for bound & ported 'EventsQueueKey' classes pickling

---
+++ tests/integration_tests/boolean_tests/events_queue_keys_tests/test_pickle.py
@@ -0,0 +1,20 @@
+import pickle
+from typing import Tuple
+
+from _martinez import EventsQueueKey as Bound
+from hypothesis import given
+
+from martinez.boolean import EventsQueueKey as Ported
+from tests.utils import are_bound_ported_events_queue_keys_equal
+from . import strategies
+
+
+@given(strategies.bound_with_ported_events_queue_keys_pairs)
+def test_round_trip(
+        bound_with_ported_events_queue_keys_pair: Tuple[Bound, Ported]
+        ) -> None:
+    bound, ported = bound_with_ported_events_queue_keys_pair
+
+    assert are_bound_ported_events_queue_keys_equal(
+        pickle.loads(pickle.dumps(bound)),
+        pickle.loads(pickle.dumps(ported)))
