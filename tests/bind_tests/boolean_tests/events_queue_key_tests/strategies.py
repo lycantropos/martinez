b41a2ac4cf664b0c71207f60c45ebd448cb6fcfd
Azat Ibrakov <azatibrakov@gmail.com>
2019-12-14 12:45:26 +0300

    Complete strategies

--- tests/bind_tests/boolean_tests/events_queue_key_tests/strategies.py
+++ tests/bind_tests/boolean_tests/events_queue_key_tests/strategies.py
@@ -1,5 +1,6 @@
 from _martinez import (EventsQueueKey,
                        Point,
+                       PolygonType,
                        SweepEvent)
 from hypothesis import strategies
 
@@ -9,14 +10,23 @@ from tests.strategies import (booleans,
                               floats,
                               make_cyclic,
                               to_bound_sweep_events)
+from tests.utils import Strategy
 
 booleans = booleans
 points = strategies.builds(Point, floats, floats)
 polygons_types = bound_polygons_types
 edges_types = bound_edges_types
-leaf_sweep_events = strategies.builds(SweepEvent, booleans, points,
-                                      strategies.none(),
-                                      bound_polygons_types, bound_edges_types)
+
+
+def to_sweep_events_with_same_polygon_type(polygon_type: PolygonType
+                                           ) -> Strategy[SweepEvent]:
+    return strategies.builds(SweepEvent, booleans, points,
+                             strategies.none(),
+                             strategies.just(polygon_type), bound_edges_types)
+
+
+leaf_sweep_events = (bound_polygons_types
+                     .flatmap(to_sweep_events_with_same_polygon_type))
 acyclic_sweep_events = strategies.recursive(leaf_sweep_events,
                                             to_bound_sweep_events)
 sweep_events = strategies.recursive(acyclic_sweep_events, make_cyclic)
