573c23373fe941deced844d6022fa258ecc6c308
Azat Ibrakov <azatibrakov@gmail.com>
2019-12-14 03:37:40 +0300

    Complete tests configurations

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,14 @@
+import os
+
 from hypothesis import (HealthCheck,
                         settings)
 
+on_azure_pipelines = os.getenv('TF_BUILD', False)
+on_travis_ci = os.getenv('CI', False)
 settings.register_profile('default',
+                          max_examples=(settings.default.max_examples // 4
+                                        if on_azure_pipelines or on_travis_ci
+                                        else settings.default.max_examples),
                           deadline=None,
                           suppress_health_check=[HealthCheck.filter_too_much,
                                                  HealthCheck.too_slow])
