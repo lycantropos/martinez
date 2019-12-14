f92ace5972cedc845c0e5619b0e96be71df35636
Azat Ibrakov <azatibrakov@gmail.com>
2019-12-14 16:13:26 +0300

    Add test for bound 'SweepLine.__lt__' magic method

---
+++ tests/bind_tests/boolean_tests/sweep_line_key_tests/test_lower_than.py
@@ -0,0 +1,26 @@
+from typing import Any
+
+import pytest
+from _martinez import SweepLineKey
+from hypothesis import given
+
+from tests.utils import equivalence
+from . import strategies
+
+
+@given(strategies.nested_sweep_line_keys)
+def test_irreflexivity(sweep_line_key: SweepLineKey) -> None:
+    assert not sweep_line_key < sweep_line_key
+
+
+@given(strategies.nested_sweep_line_keys,
+       strategies.nested_sweep_line_keys)
+def test_connection_with_greater_than(first_key: SweepLineKey,
+                                      second_key: SweepLineKey) -> None:
+    assert equivalence(first_key < second_key, second_key > first_key)
+
+
+@given(strategies.nested_sweep_line_keys, strategies.non_sweep_line_keys)
+def test_non_key(key: SweepLineKey, non_key: Any) -> None:
+    with pytest.raises(TypeError):
+        key < non_key
