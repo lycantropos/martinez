86bd0b68a90fdb2370d60a16a1f7155a61ee3a96
Azat Ibrakov <azatibrakov@gmail.com>
2019-12-14 04:21:40 +0300

    Add test for ported 'SweepLineKey' class initialization

---
+++ tests/port_tests/boolean_tests/sweep_line_key_tests/test_initialization.py
@@ -0,0 +1,12 @@
+from hypothesis import given
+
+from martinez.boolean import (SweepLineKey,
+                              SweepEvent)
+from . import strategies
+
+
+@given(strategies.sweep_events)
+def test_basic(event: SweepEvent) -> None:
+    result = SweepLineKey(event)
+
+    assert result.event == event
