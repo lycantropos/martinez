020ebbb43c2db9c9fd59b6884e12582829df7160
Azat Ibrakov <azatibrakov@gmail.com>
2019-12-17 13:23:21 +0300

    Complete test for ported 'Operation' class initialization

--- tests/port_tests/boolean_tests/operation_tests/test_initialization.py
+++ tests/port_tests/boolean_tests/operation_tests/test_initialization.py
@@ -6,13 +6,11 @@ from martinez.polygon import Polygon
 from . import strategies
 
 
-@given(strategies.polygons, strategies.polygons, strategies.empty_polygons,
-       strategies.operations_types)
-def test_basic(left: Polygon, right: Polygon, resultant: Polygon,
+@given(strategies.polygons, strategies.polygons, strategies.operations_types)
+def test_basic(left: Polygon, right: Polygon,
                operation_type: OperationType) -> None:
-    result = Operation(left, right, resultant, operation_type)
+    result = Operation(left, right, operation_type)
 
     assert result.left == left
     assert result.right == right
-    assert result.resultant is resultant
     assert result.type == operation_type
