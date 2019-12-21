fa6b9de8b84a5186eff6cfc1a6e34baa08d59f69
Azat Ibrakov <azatibrakov@gmail.com>
2019-12-21 18:14:01 +0300

    Add test for ported 'Operation.process_segments' method

---
+++ tests/port_tests/boolean_tests/operation_tests/test_process_segments.py
@@ -0,0 +1,26 @@
+from hypothesis import given
+
+from martinez.boolean import (Operation,
+                              SweepEvent)
+from . import strategies
+
+
+@given(strategies.operations)
+def test_basic(operation: Operation) -> None:
+    result = operation.process_segments()
+
+    assert result is None
+
+
+@given(strategies.operations)
+def test_events(operation: Operation) -> None:
+    events_before = operation.events
+
+    operation.process_segments()
+
+    events_after = operation.events
+
+    assert isinstance(events_before, list)
+    assert isinstance(events_after, list)
+    assert not events_before
+    assert all(isinstance(event, SweepEvent) for event in events_after)
