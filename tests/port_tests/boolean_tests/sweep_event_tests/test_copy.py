f1bbcad1af443a3ac84d3629b83f93fc2679c5c6
Azat Ibrakov <azatibrakov@gmail.com>
2019-12-23 19:37:16 +0300

    Complete test for ported 'SweepEvent' class copying

--- tests/port_tests/boolean_tests/sweep_event_tests/test_copy.py
+++ tests/port_tests/boolean_tests/sweep_event_tests/test_copy.py
@@ -2,16 +2,12 @@ import copy
 
 from hypothesis import given
 
-from martinez.boolean import EdgeType, PolygonType, SweepEvent
-from martinez.point import Point
+from martinez.boolean import SweepEvent
 from . import strategies
 
 
 @given(strategies.sweep_events)
 def test_shallow(sweep_event: SweepEvent) -> None:
-    sweep_event = SweepEvent(True, Point(0.12739150731790999, 179769313486232),
-                             None, PolygonType(1), EdgeType(3))
-    sweep_event.other_event = sweep_event
     result = copy.copy(sweep_event)
 
     assert result is not sweep_event
