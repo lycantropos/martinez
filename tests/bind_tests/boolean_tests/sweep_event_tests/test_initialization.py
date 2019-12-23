27e03b0949188a5d222e699ab7d68e92dc048f5f
Azat Ibrakov <azatibrakov@gmail.com>
2019-12-23 22:15:18 +0300

    Complete test for bound 'SweepEvent' class initialization

--- tests/bind_tests/boolean_tests/sweep_event_tests/test_initialization.py
+++ tests/bind_tests/boolean_tests/sweep_event_tests/test_initialization.py
@@ -11,12 +11,12 @@ from . import strategies
 
 @given(strategies.booleans, strategies.points, strategies.maybe_sweep_events,
        strategies.polygons_types, strategies.edges_types,
-       strategies.booleans, strategies.booleans)
+       strategies.booleans, strategies.booleans, strategies.booleans)
 def test_basic(is_left: bool, point: Point, other_event: Optional[SweepEvent],
                polygon_type: PolygonType, edge_type: EdgeType, in_out: bool,
-               other_in_out: bool) -> None:
+               other_in_out: bool, in_result: bool) -> None:
     result = SweepEvent(is_left, point, other_event, polygon_type, edge_type,
-                        in_out, other_in_out)
+                        in_out, other_in_out, in_result)
 
     assert result.is_left is is_left
     assert result.point == point
@@ -25,3 +25,4 @@ def test_basic(is_left: bool, point: Point, other_event: Optional[SweepEvent],
     assert result.edge_type == edge_type
     assert result.in_out is in_out
     assert result.other_in_out is other_in_out
+    assert result.in_result is in_result
