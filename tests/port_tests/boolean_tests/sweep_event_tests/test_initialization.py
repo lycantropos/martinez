b6c5eb2a918be9358b820c3b16c3658961a513aa
Azat Ibrakov <azatibrakov@gmail.com>
2019-12-23 18:59:50 +0300

    Complete test for ported 'SweepEvent' class initialization

--- tests/port_tests/boolean_tests/sweep_event_tests/test_initialization.py
+++ tests/port_tests/boolean_tests/sweep_event_tests/test_initialization.py
@@ -10,14 +10,18 @@ from . import strategies
 
 
 @given(strategies.booleans, strategies.points, strategies.maybe_sweep_events,
-       strategies.polygons_types, strategies.edges_types)
+       strategies.polygons_types, strategies.edges_types, strategies.booleans,
+       strategies.booleans)
 def test_basic(is_left: bool, point: Point, other_event: Optional[SweepEvent],
-               polygon_type: PolygonType, edge_type: EdgeType) -> None:
-    result = SweepEvent(is_left, point, other_event,
-                        polygon_type, edge_type)
+               polygon_type: PolygonType, edge_type: EdgeType, in_out: bool,
+               other_in_out: bool) -> None:
+    result = SweepEvent(is_left, point, other_event, polygon_type, edge_type,
+                        in_out, other_in_out)
 
     assert result.is_left is is_left
     assert result.point == point
     assert result.other_event is other_event
     assert result.polygon_type == polygon_type
     assert result.edge_type == edge_type
+    assert result.in_out is in_out
+    assert result.other_in_out is other_in_out
