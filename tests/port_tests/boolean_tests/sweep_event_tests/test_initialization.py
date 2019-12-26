d03afb5ce5efb476be757140b8fc320599365036
Azat Ibrakov <azatibrakov@gmail.com>
2019-12-26 18:03:36 +0300

    Complete test for ported 'SweepEvent' class initialization

--- tests/port_tests/boolean_tests/sweep_event_tests/test_initialization.py
+++ tests/port_tests/boolean_tests/sweep_event_tests/test_initialization.py
@@ -12,12 +12,14 @@ from . import strategies
 @given(strategies.booleans, strategies.points, strategies.maybe_sweep_events,
        strategies.polygons_types, strategies.edges_types, strategies.booleans,
        strategies.booleans, strategies.booleans,
-       strategies.non_negative_integers)
+       strategies.non_negative_integers, strategies.maybe_sweep_events)
 def test_basic(is_left: bool, point: Point, other_event: Optional[SweepEvent],
                polygon_type: PolygonType, edge_type: EdgeType, in_out: bool,
-               other_in_out: bool, in_result: bool, position: int) -> None:
+               other_in_out: bool, in_result: bool, position: int,
+               prev_in_result_event: Optional[SweepEvent]) -> None:
     result = SweepEvent(is_left, point, other_event, polygon_type, edge_type,
-                        in_out, other_in_out, in_result, position)
+                        in_out, other_in_out, in_result, position,
+                        prev_in_result_event)
 
     assert result.is_left is is_left
     assert result.point == point
@@ -28,3 +30,4 @@ def test_basic(is_left: bool, point: Point, other_event: Optional[SweepEvent],
     assert result.other_in_out is other_in_out
     assert result.in_result is in_result
     assert result.position == position
+    assert result.prev_in_result_event is prev_in_result_event
