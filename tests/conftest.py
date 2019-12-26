ecbb06bb2f1cd48b54116a4dd9d0b481b65d2eb1
Azat Ibrakov <azatibrakov@gmail.com>
2019-12-26 19:38:24 +0300

    Complete tests configurations

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,5 +10,6 @@ settings.register_profile('default',
                                         if on_azure_pipelines or on_travis_ci
                                         else settings.default.max_examples),
                           deadline=None,
-                          suppress_health_check=[HealthCheck.filter_too_much,
+                          suppress_health_check=[HealthCheck.data_too_large,
+                                                 HealthCheck.filter_too_much,
                                                  HealthCheck.too_slow])
