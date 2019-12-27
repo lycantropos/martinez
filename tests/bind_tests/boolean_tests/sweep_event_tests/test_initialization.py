98a648255063d10a1998f72cb092dd24dc7bc2e1
Azat Ibrakov <azatibrakov@gmail.com>
2019-12-27 12:23:21 +0300

    Complete test for bound 'SweepEvent' class initialization

--- tests/bind_tests/boolean_tests/sweep_event_tests/test_initialization.py
+++ tests/bind_tests/boolean_tests/sweep_event_tests/test_initialization.py
@@ -12,13 +12,15 @@ from . import strategies
 @given(strategies.booleans, strategies.points, strategies.maybe_sweep_events,
        strategies.polygons_types, strategies.edges_types,
        strategies.booleans, strategies.booleans, strategies.booleans,
-       strategies.unsigned_integers, strategies.maybe_sweep_events)
+       strategies.unsigned_integers, strategies.unsigned_integers,
+       strategies.maybe_sweep_events)
 def test_basic(is_left: bool, point: Point, other_event: Optional[SweepEvent],
                polygon_type: PolygonType, edge_type: EdgeType, in_out: bool,
-               other_in_out: bool, in_result: bool, position: int,
+               other_in_out: bool, in_result: bool,
+               position: int, contour_id: int,
                prev_in_result_event: Optional[SweepEvent]) -> None:
     result = SweepEvent(is_left, point, other_event, polygon_type, edge_type,
-                        in_out, other_in_out, in_result, position,
+                        in_out, other_in_out, in_result, position, contour_id,
                         prev_in_result_event)
 
     assert result.is_left is is_left
@@ -30,4 +32,5 @@ def test_basic(is_left: bool, point: Point, other_event: Optional[SweepEvent],
     assert result.other_in_out is other_in_out
     assert result.in_result is in_result
     assert result.position == position
+    assert result.contour_id == contour_id
     assert result.prev_in_result_event is prev_in_result_event
