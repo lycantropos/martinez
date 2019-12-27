48ee2b7fe29f477125c0295aa3b03e2039778e9a
Azat Ibrakov <azatibrakov@gmail.com>
2019-12-27 13:19:32 +0300

    Complete ported 'Contour.is_external' property

--- martinez/contour.py
+++ martinez/contour.py
@@ -10,13 +10,13 @@ from .point import Point
 
 
 class Contour:
-    __slots__ = ('_points', '_holes', '_is_external')
+    __slots__ = ('_points', '_holes', 'is_external')
 
     def __init__(self, points: List[Point], holes: List[int], is_external: bool
                  ) -> None:
         self._points = points
         self._holes = holes
-        self._is_external = is_external
+        self.is_external = is_external
 
     __repr__ = generate_repr(__init__)
 
@@ -30,10 +30,6 @@ class Contour:
     @property
     def holes(self) -> List[int]:
         return self._holes
-
-    @property
-    def is_external(self) -> bool:
-        return self._is_external
 
     @property
     def is_counterclockwise(self) -> bool:
@@ -58,7 +54,7 @@ class Contour:
     def __eq__(self, other: 'Contour') -> bool:
         return (self._points == other._points
                 and self._holes == other._holes
-                and self._is_external is other._is_external
+                and self.is_external is other.is_external
                 if isinstance(other, Contour)
                 else NotImplemented)
 
