6abb01a21e817d749360836dff3e8bc5f0d5904b
Azat Ibrakov <azatibrakov@gmail.com>
2019-12-27 15:34:40 +0300

    Add test for bound & ported 'Operation.connect_edges' method

---
+++ tests/integration_tests/boolean_tests/operations_tests/test_connect_edges.py
@@ -0,0 +1,29 @@
+from typing import (List,
+                    Tuple)
+
+from _martinez import (Operation as Bound,
+                       SweepEvent as BoundSweepEvent)
+from hypothesis import given
+
+from martinez.boolean import (Operation as Ported,
+                              SweepEvent as PortedSweepEvent)
+from tests.utils import (are_bound_ported_polygons_equal,
+                         are_bound_ported_sweep_events_lists_equal)
+from . import strategies
+
+
+@given(strategies.operations_with_events_lists_pairs)
+def test_basic(
+        operations_with_events_lists_pair: Tuple[Tuple[Bound, Ported],
+                                                 Tuple[List[BoundSweepEvent],
+                                                       List[PortedSweepEvent]]]
+) -> None:
+    ((bound, ported),
+     (bound_events, ported_events)) = operations_with_events_lists_pair
+
+    bound.connect_edges(bound_events)
+    ported.connect_edges(ported_events)
+
+    assert are_bound_ported_sweep_events_lists_equal(bound_events,
+                                                     ported_events)
+    assert are_bound_ported_polygons_equal(bound.resultant, ported.resultant)
