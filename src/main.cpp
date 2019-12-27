23b224875721974311f71148f5c035f5d5b297b5
Azat Ibrakov <azatibrakov@gmail.com>
2019-12-27 12:03:11 +0300

    Complete bound 'SweepEvent' class initialization

--- src/main.cpp
+++ src/main.cpp
@@ -619,11 +619,12 @@ PYBIND11_MODULE(MODULE_NAME, m) {
       m, SWEEP_EVENT_NAME)
       .def(py::init<bool, const cbop::Point_2&, cbop::SweepEvent*,
                     cbop::PolygonType, cbop::EdgeType, bool, bool, bool, size_t,
-                    cbop::SweepEvent*>(),
+                    size_t, cbop::SweepEvent*>(),
            py::arg("left"), py::arg("point"), py::arg("other_event").none(true),
            py::arg("polygon_type"), py::arg("edge_type"),
            py::arg("in_out") = false, py::arg("other_in_out") = false,
            py::arg("in_result") = false, py::arg("position") = 0,
+           py::arg("contour_id") = 0,
            py::arg("prev_in_result_event").none(true) = nullptr,
            py::return_value_policy::reference)
       .def(py::pickle(
