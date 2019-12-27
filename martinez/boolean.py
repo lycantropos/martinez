7bf4baff942bad6095f39cd18b46250f0c554ffb
Azat Ibrakov <azatibrakov@gmail.com>
2019-12-27 12:24:03 +0300

    Complete ported 'SweepEvent' class initialization

--- martinez/boolean.py
+++ martinez/boolean.py
@@ -98,7 +98,7 @@ SweepEventState = Tuple[List[PlainSweepEventState],
 class SweepEvent:
     __slots__ = ('is_left', 'point', 'other_event', 'polygon_type',
                  'edge_type', 'in_out', 'other_in_out', 'in_result',
-                 'position', 'prev_in_result_event')
+                 'position', 'contour_id', 'prev_in_result_event')
 
     def __init__(self, is_left: bool, point: Point,
                  other_event: Optional['SweepEvent'],
@@ -107,6 +107,7 @@ class SweepEvent:
                  in_out: bool = False, other_in_out: bool = False,
                  in_result: bool = False,
                  position: int = 0,
+                 contour_id: int = 0,
                  prev_in_result_event: Optional['SweepEvent'] = None) -> None:
         self.is_left = is_left
         self.point = point
@@ -117,6 +118,7 @@ class SweepEvent:
         self.other_in_out = other_in_out
         self.in_result = in_result
         self.position = position
+        self.contour_id = contour_id
         self.prev_in_result_event = prev_in_result_event
 
     def __getstate__(self) -> SweepEventState:
