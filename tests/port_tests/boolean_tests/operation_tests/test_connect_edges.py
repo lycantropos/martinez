fbe2c23b7a3a8acf5bdf213a7b2bc3d570d522c1
Azat Ibrakov <azatibrakov@gmail.com>
2019-12-27 21:00:27 +0300

    Complete test for ported 'Operation.connect_edges' method

--- tests/port_tests/boolean_tests/operation_tests/test_connect_edges.py
+++ tests/port_tests/boolean_tests/operation_tests/test_connect_edges.py
@@ -5,7 +5,6 @@ from hypothesis import given
 
 from martinez.boolean import (Operation,
                               SweepEvent)
-from tests.utils import implication
 from . import strategies
 
 
@@ -17,11 +16,3 @@ def test_basic(operation_with_events: Tuple[Operation, List[SweepEvent]]
     result = operation.connect_edges(events)
 
     assert result is None
-
-
-@given(strategies.operations_with_events_lists)
-def test_properties(operation_with_events: Tuple[Operation, List[SweepEvent]]
-                    ) -> None:
-    operation, events = operation_with_events
-
-    assert implication(bool(events), bool(operation.resultant.contours))
