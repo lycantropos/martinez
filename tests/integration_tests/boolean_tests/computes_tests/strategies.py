d5e09045c081e136c682857100168995182bae94
Azat Ibrakov <azatibrakov@gmail.com>
2019-12-28 11:23:38 +0300

    Add test for bound & ported 'compute' functions

---
+++ tests/integration_tests/boolean_tests/computes_tests/strategies.py
@@ -0,0 +1,69 @@
+from typing import Tuple
+
+from _martinez import Polygon as BoundPolygon
+from hypothesis import strategies
+
+from martinez.polygon import Polygon as PortedPolygon
+from tests.strategies import (bound_with_ported_operations_types_pairs,
+                              single_precision_floats as floats,
+                              to_bound_with_ported_contours_pair,
+                              to_bound_with_ported_contours_vertices_pair,
+                              to_bound_with_ported_polygons_pair)
+from tests.utils import (MAX_CONTOURS_COUNT,
+                         strategy_to_pairs,
+                         to_non_overlapping_bound_polygons_pair,
+                         to_non_overlapping_contours_lists,
+                         to_non_overlapping_ported_polygons_pair,
+                         transpose)
+
+operations_types_pairs = bound_with_ported_operations_types_pairs
+contours_vertices_pairs = to_bound_with_ported_contours_vertices_pair(floats)
+contours_pairs = strategies.builds(to_bound_with_ported_contours_pair,
+                                   contours_vertices_pairs,
+                                   strategies.builds(list),
+                                   strategies.just(True))
+contours_lists_pairs = (strategies.lists(contours_pairs,
+                                         max_size=MAX_CONTOURS_COUNT)
+                        .map(transpose)
+                        .map(to_non_overlapping_contours_lists))
+empty_contours_lists_pairs = strategy_to_pairs(strategies.builds(list))
+non_empty_contours_lists_pairs = (strategies.lists(contours_pairs,
+                                                   min_size=1,
+                                                   max_size=1)
+                                  .map(transpose)
+                                  .map(to_non_overlapping_contours_lists))
+polygons_pairs = strategies.builds(to_bound_with_ported_polygons_pair,
+                                   contours_lists_pairs)
+empty_polygons_pairs = strategies.builds(to_bound_with_ported_polygons_pair,
+                                         empty_contours_lists_pairs)
+non_empty_polygons_pairs = strategies.builds(
+        to_bound_with_ported_polygons_pair,
+        non_empty_contours_lists_pairs)
+
+
+def to_non_overlapping_polygons_pairs(first_pairs_pair: Tuple[BoundPolygon,
+                                                              PortedPolygon],
+                                      second_pairs_pair: Tuple[BoundPolygon,
+                                                               PortedPolygon]
+                                      ) -> Tuple[Tuple[BoundPolygon,
+                                                       PortedPolygon],
+                                                 Tuple[BoundPolygon,
+                                                       PortedPolygon]]:
+    first_bound_polygon, first_ported_polygon = first_pairs_pair
+    second_bound_polygon, second_ported_polygon = second_pairs_pair
+    bound_polygons_pair = to_non_overlapping_bound_polygons_pair(
+            first_bound_polygon,
+            second_bound_polygon)
+    ported_polygons_pair = to_non_overlapping_ported_polygons_pair(
+            first_ported_polygon,
+            second_ported_polygon)
+    return transpose([bound_polygons_pair, ported_polygons_pair])
+
+
+polygons_pairs_pairs = (strategies.tuples(empty_polygons_pairs,
+                                          non_empty_polygons_pairs)
+                        | strategies.tuples(non_empty_polygons_pairs,
+                                            empty_polygons_pairs)
+                        | strategies.builds(to_non_overlapping_polygons_pairs,
+                                            non_empty_polygons_pairs,
+                                            non_empty_polygons_pairs))
---
+++ tests/integration_tests/boolean_tests/computes_tests/test_call.py
@@ -0,0 +1,27 @@
+from typing import Tuple
+
+from _martinez import (OperationType as BoundOperationType,
+                       Polygon as BoundPolygon,
+                       compute as bound)
+from hypothesis import given
+
+from martinez.boolean import (OperationType as PortedOperationType,
+                              Polygon as PortedPolygon,
+                              compute as ported)
+from tests.utils import are_bound_ported_polygons_equal
+from . import strategies
+
+
+@given(strategies.polygons_pairs_pairs, strategies.operations_types_pairs)
+def test_basic(polygons_pairs_pair: Tuple[Tuple[BoundPolygon, PortedPolygon],
+                                          Tuple[BoundPolygon, PortedPolygon]],
+               operations_types_pair: Tuple[BoundOperationType,
+                                            PortedOperationType]) -> None:
+    ((bound_left, ported_left),
+     (bound_right, ported_right)) = polygons_pairs_pair
+    bound_operation_type, ported_operation_type = operations_types_pair
+
+    bound_result = bound(bound_left, bound_right, bound_operation_type)
+    ported_result = ported(ported_left, ported_right, ported_operation_type)
+
+    assert are_bound_ported_polygons_equal(bound_result, ported_result)
